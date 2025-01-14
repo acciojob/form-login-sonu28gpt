53a20c971c934758c415710bbe2f030cda3f0686
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-15 00:55:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	// e.preventDefault();
+	let form=document.getElementById('form1');
+	let formData=new FormData(form);
+	let {fname='',lname=''}=Object.fromEntries(formData);
+	alert(fname+' '+lname);
+	
 
 }
